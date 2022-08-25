744c92ed586fb06525256957024ee2c60958245e
shierone <dellabatemarco@gmail.com>
2022-08-25 13:53:24 +0200

    added I/O for json parameters

--- src/main/kotlin/Structure.kt
+++ src/main/kotlin/Structure.kt
@@ -9,20 +9,21 @@ import org.openrndr.extra.parameters.IntParameter
 import org.openrndr.extra.timeoperators.LFO
 import org.openrndr.math.*
 import org.openrndr.shape.*
+import kotlin.math.ceil
 import kotlin.math.sin
 
 class Structure(gui: GUI) {
 
     private val structureSliders = object {
 
-        @DoubleParameter("Height", 5.0, 960.0)
+        @DoubleParameter("Height", 5.0, 1920.0)
         var height = 640.0
 
         @IntParameter("Height Segments", 10, 250)
         var heightSegments = 100
 
 
-        @DoubleParameter("Width", 5.0, 540.0)
+        @DoubleParameter("Width", 5.0, 1080.0)
         var width = 540.0
 
         @IntParameter("Width / Rotation Segments", 10, 100)
@@ -30,7 +31,7 @@ class Structure(gui: GUI) {
 
     }.addTo(gui, "Structure")
     private val vertebraeSliders = object {
-        @DoubleParameter("Visibility", 0.0, 1.0)
+        @DoubleParameter("Visibility", 0.05, 1.0)
         var visibility = 0.5
 
         @DoubleParameter("Wave amount", 0.0, 1.0)
@@ -54,23 +55,8 @@ class Structure(gui: GUI) {
         @DoubleParameter("Noise Frequency", 0.0, 0.5)
         var noiseFrequency = 0.005
 
-        @DoubleParameter("rotationX", 0.0, 60.0)
-        var rotX = 60.0
-
-        @DoubleParameter("rotationY", 0.01, 360.0)
-        var rotY = 360.0
-
-        @DoubleParameter("rotationZ", 0.01, 50.0)
-        var rotZ = 50.0
-
 
     }.addTo(gui, "Vertebrae")
-    private val spineSliders = object {
-
-        @DoubleParameter("Spines amount", 0.0, 1.0)
-        var spines = 0.0
-
-    }.addTo(gui, "Spines")
     private val cellSliders = object {
 
         @DoubleParameter("Cells amount", 0.0, 1.0)
@@ -83,17 +69,23 @@ class Structure(gui: GUI) {
 
     private val thickLine = ThickLine(gui)
 
-    fun draw(drawer: Drawer, t: Double, lfo: LFO, dimensions: Double = 2.0, palette: List<List<ColorRGBa>>) {
 
-            drawer.translate(0.0, (960.0 - structureSliders.height) / 2.0)
+    fun draw(drawer: Drawer, t: Double, lfo: LFO, complexity: Double, dimensions: Double = 2.0, palette: List<List<ColorRGBa>>) {
+
             val rows = mutableListOf<List<Vector3>>()
 
-        // VERTEBRAE
+            // ADAPTIVE VALUES
+            val heightSegments = ceil(structureSliders.heightSegments.times(complexity)).toInt().coerceAtLeast(5)
+            val rotationSegments = ceil(structureSliders.rotationSegments.times(complexity)).toInt().coerceAtLeast(5)
+
+            drawer.translate(0.0, -structureSliders.height / 2.0)
+
+            // VERTEBRAE
             drawer.isolated {
-                for(y in 0 until structureSliders.heightSegments) {
+                for(y in 0 until heightSegments) {
                     val f = vertebraeSliders.wavePhase
                     val ph = vertebraeSliders.waveFrequency / 500.0 * y
-                    val yOffset = structureSliders.height / structureSliders.heightSegments * y
+                    val yOffset = structureSliders.height / heightSegments * y
 
                     val waveforms = listOf(
                         lfo.sine(f, ph),
@@ -108,21 +100,15 @@ class Structure(gui: GUI) {
                     val sM = simplex(398, yOffset * 0.001 + t * 0.005) * 0.5 + 0.5
                     val subbedContour = contour.sub( sM * t * 0.075, sM * t * 0.075 + vertebraeSliders.visibility)
 
-                    val row = subbedContour.equidistantPositions(structureSliders.rotationSegments).mapIndexed { i, it ->
+
+                    val row = subbedContour.equidistantPositions(rotationSegments).mapIndexed { i, it ->
                         val scale = 0.01 * vertebraeSliders.noiseScale
                         val n = (simplex(2844, it.x * scale, it.y * scale, t * 0.005 + y * vertebraeSliders.noiseFrequency) * 0.5 + 0.5) * vertebraeSliders.noise
                         val n2 = simplex(394, i * 0.009 + y * 0.05) * 20.0
                         it.mix(Vector2.ZERO, n).vector3(y = yOffset + n2, z = it.y * (dimensions - 2.0))
                     }
 
-/*                    drawer.translate(width / 2.0, yOffset)
-                    val nRotation = simplex(2903, yOffset * 0.005 + t)
-                    drawer.rotate(Vector3.UNIT_Z, (y.toDouble() / structureSliders.heightSegments.toDouble()) * nRotation * vertebraeSliders.rotX)
-                    drawer.rotate(Vector3.UNIT_Y, (y.toDouble() / structureSliders.heightSegments.toDouble()) * nRotation * vertebraeSliders.rotY)
-                    drawer.rotate(Vector3.UNIT_X, (y.toDouble() / structureSliders.heightSegments.toDouble()) * nRotation * vertebraeSliders.rotZ)
-                    drawer.translate(-width / 2.0, -yOffset)*/
-
-                    thickLine.write(t, row, structureSliders.rotationSegments, palette, isRow = true)
+                    thickLine.write(t, row, rotationSegments, palette, isRow = true)
                     if(vertebraeSliders.visibility > 0.0) {
                         thickLine.draw(drawer, isRow = true)
                     }
@@ -135,61 +121,75 @@ class Structure(gui: GUI) {
 
 
             // SPINES
-/*            drawer.isolated {
-                for(r in 0 until structureSliders.rotationSegments) {
+            drawer.isolated {
+                for(r in 0 until rotationSegments) {
                     val col = mutableListOf<Vector3>()
                     for(row in rows) {
                         col.add(row[r])
                     }
 
-                    thickLine.write(t, col, structureSliders.heightSegments, palette, isRow = false)
+                    thickLine.write(t, col, heightSegments, palette, isRow = false)
                     thickLine.draw(drawer, isRow = false)
                 }
 
-            }*/
+            }
 
 
- /*
-            // CORNERS (only for 2d)
+
+            // CELLS (only for 2d)
             if(dimensions < 2.1 && rows.size > 0) {
                 drawer.isolated {
-                    if(cornerSliders.cellType == 0) {
-                        drawer.circles {
-                            for(row in rows){
-                                for(vertex in row) {
+                    drawer.depthWrite = false
+                    drawer.depthTestPass = DepthTestPass.ALWAYS
+                    when (cellSliders.cellType) {
+                        0 -> drawer.circles {
+                                for(row in rows){
+                                    for(vertex in row) {
 
-                                    val n = simplex(244, vertex.x * 0.003 + t * 0.3, vertex.y * 0.004 + t * 0.33) * 0.5 + 0.5
-                                    val mn = n * 10.0 * cornerSliders.corners
+                                        val n = simplex(244, vertex.x * 0.003 + t * 0.3, vertex.y * 0.004 + t * 0.33) * 0.5 + 0.5
+                                        val mn = n * 10.0 * cellSliders.corners
 
-                                    this.fill = palette[0][0].mix(palette[1][0], n)
+                                        this.fill = palette[0][0].mix(palette[1][0], n)
 
-                                    this.circle(vertex.xy, ceil(mn * cornerSliders.corners * 5.0))
+                                        this.circle(vertex.xy, ceil(mn * cellSliders.corners * 5.0))
+                                    }
                                 }
                             }
-                        }
-                    }
-                    else if (cornerSliders.cellType == 1) {
-                        drawer.rectangles {
-                            for(row in rows){
-                                for(vertex in row) {
+                        1 ->  drawer.rectangles {
+                                for(row in rows){
+                                    for(vertex in row) {
 
-                                    val n = simplex(244, vertex.x * 0.003 + t * 0.3, vertex.y * 0.004 + t * 0.33) * 0.5 + 0.5
-                                    val mn = n * 10.0 * cornerSliders.corners
+                                        val n = simplex(244, vertex.x * 0.003 + t * 0.3, vertex.y * 0.004 + t * 0.33) * 0.5 + 0.5
+                                        val mn = n * 10.0 * cellSliders.corners
 
-                                    this.fill = palette[0][0].mix(palette[0][0], n)
+                                        this.fill = palette[0][0].mix(palette[0][0], n)
 
-                                    this.rectangle(Rectangle.fromCenter(vertex.xy, ceil(mn * cornerSliders.corners * 5.0)))
+                                        this.rectangle(Rectangle.fromCenter(vertex.xy, ceil(mn * cellSliders.corners * 5.0)))
+                                    }
                                 }
                             }
-                        }
-                    }
-                    else if (cornerSliders.cellType == 2) {
+                        2 -> drawer.rectangles { // do it with vertexbuffer?
+                                for(row in rows){
+                                    for(vertex in row) {
+
+                                        val n = simplex(244, vertex.x * 0.003 + t * 0.3, vertex.y * 0.004 + t * 0.33) * 0.5 + 0.5
+                                        val mn = n * 10.0 * cellSliders.corners
+
+                                        this.fill = palette[0][0].mix(palette[0][0], n)
+
+                                        this.rectangle(Rectangle(vertex.xy, 8.0,  cellSliders.corners * 20.0))
+                                    }
+                                }
+                            }
+
+
 
                     }
                 }
             }
-*/
+
 
 
     }
 }
+
