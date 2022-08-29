61d617ec8ae834fda60b0e02e350956c30c7ed3a
Edwin Jakobs <edwin@rndr.studio>
2022-08-29 18:24:22 +0200

    Restricted color palette, improved fx pipeline, json files i/o in coroutine

---
+++ src/main/kotlin/CoverGenWithoutGUI/Ecosystem.kt
@@ -0,0 +1,130 @@
+package CoverGenWithoutGUI
+
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.*
+import org.openrndr.extra.gui.GUI
+import org.openrndr.extra.gui.addTo
+import org.openrndr.extra.noise.Random
+import org.openrndr.extra.noise.simplex
+import org.openrndr.extra.parameters.DoubleParameter
+import org.openrndr.extra.parameters.IntParameter
+import org.openrndr.extra.parameters.XYParameter
+import org.openrndr.extra.timeoperators.LFO
+import org.openrndr.extra.camera.Orbital
+import org.openrndr.math.Vector2
+import org.openrndr.math.Vector3
+import org.openrndr.math.clamp
+import org.openrndr.shape.Rectangle
+import kotlin.math.atan2
+import kotlin.math.sin
+
+class Ecosystem (gui: GUI, val frame: Rectangle){
+
+    private val cameraSliders = object {
+        @DoubleParameter("Dimensions", 2.0, 3.0)
+        var dimensions = 1.0
+
+        @DoubleParameter("Camera Y Angle", 0.0, 180.0)
+        var yAngle = 90.0
+
+        @DoubleParameter("Camera X Angle", 0.0, 180.0)
+        var xAngle = 180.0
+
+    }.addTo(gui, "Camera Settings")
+    private val ecosystemSliders = object {
+
+        @IntParameter("N Structures", 1, 120)
+        var nStructures = 1
+
+        @DoubleParameter("Angle", 0.0, 360.0)
+        var angle = 0.0
+
+        @DoubleParameter("Concentricity", 0.0, 1.0)
+        var concentricity = 0.0
+
+        @DoubleParameter("Random angle", 0.0, 1.0)
+        var randomAngle = 0.0
+
+        @XYParameter("Attractor position")
+        var attractorPos = frame.center
+
+        @DoubleParameter("Rotation movement", 0.0, 1.0)
+        var rotMovement = 0.5
+
+    }.addTo(gui, "Ecosystem Settings")
+
+
+    val orb = Orbital().apply {
+        eye = Vector3(0.0, 0.0, 0.01)
+        dampingFactor = 0.0
+        near = 0.5
+        far = 5000.0
+        userInteraction = false
+    }
+    var structure = Structure(gui)
+    var lfo = LFO()
+
+    private val dry = renderTarget(frame.width.toInt(), frame.height.toInt()) {
+        colorBuffer()
+        depthBuffer()
+    }
+    val dryResolved = colorBuffer(frame.width.toInt(), frame.height.toInt())
+
+
+
+    fun draw(drawer: Drawer, seconds: Double, palette: List<List<ColorRGBa>>){
+
+        // the higher the nStructures, the lower the complexity
+        val complexity = (1.0 / ecosystemSliders.nStructures).clamp(0.05, 1.0)
+
+        val positions = (0 until ecosystemSliders.nStructures).map {
+            val pos = Random.point(frame.offsetEdges(40.0)).mix(frame.center, it.toDouble() / ecosystemSliders.nStructures.toDouble())
+            val angle = Random.double(130.0)
+            pos to angle
+        }
+
+        dry.clearColor(0, ColorRGBa.TRANSPARENT)
+
+        drawer.rotate(Vector3.UNIT_X, 180.0)
+        drawer.translate(-frame.center)
+
+
+        for((pos, angle) in positions) {
+
+            val fixedPos = if(positions.size == 1) frame.center else pos
+
+            drawer.isolatedWithTarget(dry) {
+
+
+                if(positions.size == 1) {
+                    drawer.clear(ColorRGBa.TRANSPARENT)
+                }
+                dry.clearDepth(1.0)
+
+
+                drawer.translate(fixedPos.x, fixedPos.y, sin(fixedPos.x) * frame.height)
+
+                if(ecosystemSliders.nStructures > 1) { // ecosystem movement
+                    val attractor =  ecosystemSliders.attractorPos * Vector2(frame.width, frame.height) + frame.center
+                    val angleToCenter = Math.toDegrees(atan2(fixedPos.y - attractor.y, fixedPos.x -  attractor.x))
+                    val randomAngle =  angle * ecosystemSliders.randomAngle
+                    val rotationMovement = (simplex(335, fixedPos.x * 0.004, fixedPos.y * 0.004 + seconds * 0.05) * 0.5 + 0.5) * ecosystemSliders.rotMovement
+
+
+                    drawer.rotate(angleToCenter * ecosystemSliders.concentricity)
+                    drawer.rotate(randomAngle + rotationMovement * 60.0)
+                }
+                drawer.rotate(ecosystemSliders.angle)
+                drawer.scale(complexity * 1.1 + 0.075)
+
+                orb.camera.rotateTo(cameraSliders.xAngle, cameraSliders.yAngle)
+
+                structure.draw(drawer, seconds, lfo, complexity, cameraSliders.dimensions, palette)
+
+            }
+        }
+        dry.colorBuffer(0).copyTo(dryResolved)
+
+        drawer.defaults()
+    }
+}
---
+++ src/main/kotlin/CoverGenWithoutGUI/ThickLine.kt
@@ -0,0 +1,127 @@
+package CoverGenWithoutGUI
+
+import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.*
+import org.openrndr.extra.gui.GUI
+import org.openrndr.extra.gui.addTo
+import org.openrndr.extra.noise.Random
+import org.openrndr.extra.noise.simplex
+import org.openrndr.extra.parameters.DoubleParameter
+import org.openrndr.math.Vector3
+import org.openrndr.shape.Path3D
+import kotlin.math.PI
+import kotlin.math.cos
+
+class ThickLine(gui: GUI) {
+
+    var columnVb: VertexBuffer? = null
+    var rowVb: VertexBuffer? = null
+    private var columnContours = object {
+
+        @DoubleParameter("Thickness", 0.0, 20.0)
+        var thickness = 0.5
+
+        @DoubleParameter("Pen pressure", 0.0, 1.0)
+        var penPressure = 1.0
+
+        @DoubleParameter("Turbulence Amount", 0.0, 1.0)
+        var turbulenceAmount = 0.5
+
+        @DoubleParameter("Turbulence Scale", 0.001, 0.08)
+        var turbulenceScale = 0.001
+
+    }.addTo(gui, "ThickLine / Vertical")
+    private var rowContours = object {
+
+        @DoubleParameter("Thickness", 0.0, 20.0)
+        var thickness = 0.5
+
+        @DoubleParameter("Pen pressure", 0.0, 1.0)
+        var penPressure = 1.0
+
+        @DoubleParameter("Turbulence Amount", 0.0, 1.0)
+        var turbulenceAmount = 0.5
+
+        @DoubleParameter("Turbulence Scale", 0.001, 0.08)
+        var turbulenceScale = 0.001
+
+    }.addTo(gui, "Contour / Horizontal")
+
+
+    fun write(t: Double, vertexes: List<Vector3>, nSegments: Int, palette: List<List<ColorRGBa>>, isRow: Boolean) {
+        Random.resetState()
+
+
+        val tone1 = if (isRow) palette[0][1] else palette[1][2]
+        val tone2 = if (isRow) palette[1][1] else palette[2][2]
+
+
+        var tempBuffer = vertexBuffer(vertexFormat {
+            position(3)
+            color(4)
+        }, nSegments * 2).apply {
+
+            val penPressure = if (isRow) rowContours.penPressure else columnContours.penPressure
+            val thickness = if (isRow) rowContours.thickness else columnContours.thickness
+            val turbulenceAmount = if(isRow) rowContours.turbulenceAmount else columnContours.turbulenceAmount
+            val turbulenceScale = if(isRow) rowContours.turbulenceScale else columnContours.turbulenceScale
+
+            put {
+                vertexes.forEachIndexed { i, vertex ->
+
+                    val pc = i.toDouble() / nSegments.toDouble()
+
+                    val color = tone1.mix(tone2, pc)
+                    val turbulence = (simplex(435, vertex * turbulenceScale + t * turbulenceScale) * 0.5 + 0.5) * turbulenceAmount
+                    val pp = thickness * cos(pc * PI * 2) * penPressure
+                    val finalStrokeWeight = thickness - pp - (thickness / 2 * turbulence)
+
+                    val vertex1 = if(isRow) vertex.copy(y = vertex.y - (rowContours.thickness * finalStrokeWeight))
+                                  else vertex.copy(x = vertex.x - (columnContours.thickness * finalStrokeWeight))
+
+                    val vertex2 = if(isRow) vertex.copy(y = vertex.y + (rowContours.thickness * finalStrokeWeight))
+                                  else vertex.copy(x = vertex.x + (columnContours.thickness * finalStrokeWeight))
+
+                    write(vertex1)
+                    write(color.toVector4())
+                    write(vertex2)
+                    write(color.toVector4())
+
+                }
+
+            }
+
+        }
+
+
+        if(isRow) {
+            rowVb = tempBuffer
+        } else {
+            columnVb = tempBuffer
+        }
+
+
+    }
+
+    fun draw(drawer: Drawer, isRow: Boolean) {
+
+
+        drawer.shadeStyle = shadeStyle {
+            fragmentTransform = "x_fill = va_color;"
+        }
+
+        if(isRow) {
+
+            drawer.vertexBuffer(rowVb!!, DrawPrimitive.TRIANGLE_STRIP)
+            rowVb!!.destroy()
+
+        } else {
+
+            drawer.vertexBuffer(columnVb!!, DrawPrimitive.TRIANGLE_STRIP)
+            columnVb!!.destroy()
+
+        }
+
+
+    }
+}
