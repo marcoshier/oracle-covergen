0a197f9b841dbe11b5aa10cbe9db686fa630b733
Edwin Jakobs <edwin@rndr.studio>
2022-09-02 10:39:23 +0200

    Improve cover projections

--- src/main/kotlin/fillin/000_JsonToVectorBatch.kt
+++ src/main/kotlin/fillin/000_JsonToVectorBatch.kt
@@ -1,17 +1,19 @@
 package tools
 
+import fillin.protoJsonPath
 import java.io.File
 
 fun main() {
-    val output = File("cover-data.csv")
+    val output = File("offline-data/resolved/proto-normalized.csv")
     val writer = output.bufferedWriter()
-    val files = File("offline-data/cover-jsons").listFiles().filter { !it.isHidden && it.extension == "json" }
+    val files = File(protoJsonPath).listFiles().filter { !it.isHidden && it.extension == "json" }
     for (file in files) {
         val v = jsonToNormalizedVector(file.readText())
         println(v.size)
         writer.write(v.joinToString(","))
         writer.newLine()
     }
+    println("${files.size} files processed")
     writer.close()
 
 }
