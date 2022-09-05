9bb215f77c034d68be43c37c12a5cd49db38a1ff
Edwin Jakobs <edwin@rndr.studio>
2022-09-05 18:52:38 +0200

    Add QuaternionCamera

---
+++ src/main/kotlin/extensions/Camera2D.kt
@@ -0,0 +1,29 @@
+package extensions
+
+import org.openrndr.Extension
+import org.openrndr.Program
+import org.openrndr.draw.Drawer
+import org.openrndr.math.Matrix44
+import org.openrndr.math.transforms.buildTransform
+
+class Camera2D : Extension {
+    override var enabled = true
+    var view = Matrix44.IDENTITY
+    override fun setup(program: Program) {
+        program.mouse.dragged.listen {
+            view = buildTransform { translate(it.dragDisplacement) } * view
+        }
+        program.mouse.scrolled.listen {
+            val scaleFactor = 1.0 - it.rotation.y * 0.03
+            view = buildTransform {
+                translate(it.position)
+                scale(scaleFactor)
+                translate(-it.position)
+            } * view
+        }
+    }
+
+    override fun beforeDraw(drawer: Drawer, program: Program) {
+        drawer.view = view
+    }
+}
